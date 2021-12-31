a698067d658463b5b4b996ce39bdfa5f9d2a6408
dd <dd@pop-os.localdomain>
2021-12-31 19:03:01 +0200

    fix

--- catalog/load_fixtures.py
+++ catalog/load_fixtures.py
@@ -8,6 +8,10 @@ def create_categories(data,parent = None):
 	for item in data:
 		parent = Category.objects.create(name=item.get('name'),type=item.get('type'),class_icon=item.get('class'))
 
+		if parent:
+			parent.parent = parent
+			parent.save()
+
 	if item.get('children'):
 		create_categories(item.get('children'), parent)
 
